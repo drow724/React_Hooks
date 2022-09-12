f5607ebbf33ed7363f99f3f6fc36a81c84bdb7f3
Macintosh <drow724@naver.com>
2022-09-12 15:19:50 +0900

    useInput

--- react_hooks/src/App.js
+++ react_hooks/src/App.js
@@ -1,51 +1,21 @@
 import React, { useState } from "react";
 
-const App = () => {
-  const [item, setItem] = useState(1);
-  const incrementItem = () => setItem(item + 1);
-  const decrementItem = () => setItem(item - 1);
+const useInput = (initialValue) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    console.log(event.target);
+  };
+  return { value };
+};
 
+const App = () => {
+  const name = useInput("Mr.");
   return (
     <div className="App">
-      <h1>Hello {item}</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <button onClick={incrementItem}>IncrementItem</button>
-      <button onClick={decrementItem}>DecrementItem</button>
+      <h1>Hello</h1>
+      <input placeholder="Name" {...name} />
     </div>
   );
 };
 
-class AppUgly extends React.Component {
-  state = {
-    item: 1
-  };
-  incrementItem = () => {
-    this.setState((state) => {
-      return {
-        item: state.item + 1
-      };
-    });
-  };
-
-  decrementItem = () => {
-    this.setState((state) => {
-      return {
-        item: state.item - 1
-      };
-    });
-  };
-
-  render() {
-    const { item } = this.state;
-    return (
-      <div className="App">
-        <h1>Hello {item}</h1>
-        <h2>Start editing to see some magic happen!</h2>
-        <button onClick={this.incrementItem}>IncrementItem</button>
-        <button onClick={this.decrementItem}>DecrementItem</button>
-      </div>
-    );
-  }
-}
-
-export default AppUgly;
+export default App;
