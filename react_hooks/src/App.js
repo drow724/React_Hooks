f3930e2e3771bbd377aa62428579c7217b1258c8
Macintosh <drow724@naver.com>
2022-09-12 18:55:59 +0900

    useNotification

--- react_hooks/src/App.js
+++ react_hooks/src/App.js
@@ -1,53 +1,32 @@
 import { useEffect, useRef, useState } from "react";
 
-const useFullscreen = (callback) => {
-  const element = useRef();
-  const runCb = (isFull) => {
-    if (callback && typeof callback === "function") callback(isFull);
-  };
-
-  const triggerFull = () => {
-    if (element.current) {
-      if (element.current.requestFullscreen) {
-        element.current.requestFullscreen();
-      } else if (element.current.mozRequestFullscreen) {
-        element.current.mozRequestFullscreen();
-      } else if (element.current.webkitRequestFullscreen) {
-        element.current.webkitRequestFullscreen();
-      } else if (element.current.msRequestFullscreen) {
-        element.current.msRequestFullscreen();
-      }
-
-      runCb(true);
+const useNotification = (title, options) => {
+  if (!("Notification" in window)) {
+    return;
+  }
+  const fireNotif = () => {
+    if (Notification.permission !== "granted") {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          new Notification(title, options);
+        } else {
+          return;
+        }
+      });
+    } else {
+      new Notification(title, options);
     }
   };
-  const exitFull = () => {
-    if (document.exitFullscreen) {
-      document.exitFullscreen();
-    } else if (document.mozCancelFullscreen) {
-      document.mozExitFullscreen();
-    } else if (document.webkitExitFullscreen) {
-      document.webkitExitFullscreen();
-    } else if (document.msExitFullscreen) {
-      document.msExitFullscreen();
-    }
-    runCb(false);
-  };
-  return { element, triggerFull, exitFull };
+  return fireNotif;
 };
 
 const App = () => {
-  const onFullS = (isFull) => {
-    console.log(isFull ? "We are Full" : "We are small");
-  };
-  const { element, triggerFull, exitFull } = useFullscreen(onFullS);
+  const triggerNotif = useNotification("Can I steal your kimchi", {
+    body: "I love kimchi dont you"
+  });
   return (
     <div className="App" style={{ height: "1000vh" }}>
-      <div ref={element}>
-        <img src="https://i.ibb.co/R6RwNxx/grape.jpg" alt="grape" width="250" />
-        <button onClick={exitFull}>exitFull</button>
-      </div>
-      <button onClick={triggerFull}>Make Fullscreen</button>
+      <button onClick={triggerNotif}>Hello</button>
     </div>
   );
 };
