5506a594f2e002c55593352459c2ea7e67f33340
Macintosh <drow724@naver.com>
2022-09-12 17:36:10 +0900

    useFadeIn

--- react_hooks/src/App.js
+++ react_hooks/src/App.js
@@ -1,24 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
-const useBeforeLeave = (onBefore) => {
+const useFadeIn = (duration = 1, delay = 0) => {
+  const element = useRef();
   useEffect(() => {
-    if (typeof onBefore !== "function") {
+    if (typeof duration !== "number" || typeof delay !== "number") {
       return;
     }
-    const handle = (event) => {
-      const { clientY } = event;
-      if (clientY <= 0) onBefore();
-      //onBefore();
-    };
-    document.addEventListener("mouseleave", handle);
-    return () => document.removeEventListener("mouseleave", handle);
+    if (element.current) {
+      const { current } = element;
+      current.style.transition = `opacity ${duration}s ease-in-out ${delay}s`;
+      current.style.opacity = 1;
+    }
   }, []);
+  return { ref: element, style: { opacity: 0 } };
 };
 
 const App = () => {
-  const begForLife = () => console.log("please dont leave");
-  useBeforeLeave(begForLife);
-  return <div className="App"></div>;
+  const fadeInH1 = useFadeIn(1, 2);
+  const fadeInP = useFadeIn(5, 10);
+  return (
+    <div className="App">
+      <h1 {...fadeInH1}>Hello</h1>
+      <p {...fadeInP}>dfasdfasdfadsfadsf</p>
+    </div>
+  );
 };
 
 export default App;
