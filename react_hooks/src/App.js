37260fc3298a1f143465ebf8aa01a67339956043
Macintosh <drow724@naver.com>
2022-09-12 20:03:44 +0900

    Publishing to NPM

--- react_hooks/src/App.js
+++ react_hooks/src/App.js
@@ -1,12 +1,9 @@
-import useAxios from "./useAxios";
+import useTitle from "@jaegeun/use-title";
 
 const App = () => {
-  const {loading, data, error, refetch} = useAxios({url: "https://yts.mx/api/v2/list_movies.json"});
+  useTitle("I love NC");
   return (
     <div className="App" style={{ height: "1000vh" }}>
-      <h1>{data && data.status}</h1>
-      <h2>{loading && "loading"}</h2>
-      <button onClick={refetch}>Refetch</button>
     </div>
   );
 };
